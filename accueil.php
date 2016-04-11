09b5778317400fda452b7a5e86586bcb4a38fd50
cubouyaka <cubouyaka@laposte.net>
2016-04-11 21:14:36 +0200

    voila le projet

---
+++ accueil.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head> 
+	<link type="text/css" href="accueil.css" rel="stylesheet">
+	<meta charset="UTF-8"> 
+	<title>Accueil</title>
+</head>
+
+<body>
+	<div id="button-ctrl"></div>
+
+	<script type="text/javascript" src="jquery.js">
+	$(document).ready(function(){
+
+			alert("jQuery est prêt !");
+
+		});
+	</script>
+</body>
